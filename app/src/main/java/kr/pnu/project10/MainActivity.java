7d9e91ec571f144abaf44ff2669989d5e01c82e6
Arogya Kharel <kharelarogya@hotmail.com>
2021-04-09 01:31:44 +0900

    Political Compass

--- app/src/main/java/kr/pnu/project10/MainActivity.java
+++ app/src/main/java/kr/pnu/project10/MainActivity.java
@@ -3,6 +3,10 @@ package kr.pnu.project10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout top_left = (LinearLayout) findViewById(R.id.top_left);
+
+        TextView text1 = (TextView) top_left.getChildAt(0);
+
+        top_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), text1.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
