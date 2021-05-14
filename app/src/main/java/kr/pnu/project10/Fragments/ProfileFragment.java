2d666362951447bfe00cacf9853ffca56ce42d3c
Arogya Kharel <kharelarogya@hotmail.com>
2021-05-14 11:53:10 +0900

    Added Picasso library for retrieving images.
    ProfileFragment now contains logic for retrieving logged in user's name and profile image.
    Also added logout functionality.

--- app/src/main/java/kr/pnu/project10/Fragments/ProfileFragment.java
+++ app/src/main/java/kr/pnu/project10/Fragments/ProfileFragment.java
@@ -3,6 +3,7 @@ package kr.pnu.project10.Fragments;
 import androidx.activity.OnBackPressedCallback;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,21 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import kr.pnu.project10.Fragments.ViewModels.ProfileViewModel;
 import kr.pnu.project10.R;
@@ -25,17 +34,21 @@ import kr.pnu.project10.databinding.ProfileFragmentBinding;
 
 public class ProfileFragment extends Fragment {
 
+    private static final String TAG = "ProfileFragment";
+
     private ProfileViewModel mViewModel;
     private ProfileFragmentBinding binding;
-
-    private TextView accountTest;
+    private NavController navController;
 
     private FirebaseUser authUser;
     private FirebaseAuth mAuth;
 
-    private NavController navController;
+    private ImageView ivProfileImage;
+    private TextView tvProfileName;
+    private ImageButton btnSettings;
 
-    private String authUserID;
+    private Uri uriProfileImage;
+    private String userName;
 
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
@@ -47,7 +60,9 @@ public class ProfileFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         binding = ProfileFragmentBinding.inflate(inflater, container, false);
-        accountTest = binding.accountTest;
+        tvProfileName = binding.tvProfileName;
+        ivProfileImage = binding.ivProfileImage;
+        btnSettings = binding.btnSettings;
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -55,9 +70,11 @@ public class ProfileFragment extends Fragment {
 
         if (mAuth.getCurrentUser() == null)
             navController.navigate(R.id.signup_dest);
-        else
-            authUserID = mAuth.getCurrentUser().getEmail();
-
+        else {
+            authUser = mAuth.getCurrentUser();
+            uriProfileImage = authUser.getPhotoUrl();
+            userName = authUser.getDisplayName();
+        }
 
         return binding.getRoot();
     }
@@ -66,7 +83,19 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        accountTest.setText(authUserID);
+        tvProfileName.setText(String.valueOf(userName));
+        Log.d(TAG, "Uri is :" + userName);
+        picassoImageSet(uriProfileImage);
+
+        if (authUser != null)
+        {
+            btnSettings.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    signOut();
+                }
+            });
+        }
     }
 
     @Override
@@ -76,5 +105,34 @@ public class ProfileFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    private void picassoImageSet(Uri imgUri) {
+        Picasso.get()
+                .load(imgUri)
+                .fit()
+                .centerInside()
+                .into(ivProfileImage, new Callback.EmptyCallback() {
+                    @Override
+                    public void onSuccess() {
+                        super.onSuccess();
+                        Log.d(TAG, "Picasso SUCCESS");
+                    }
+                });
+    }
+
+    private void signOut(){
+        mAuth.signOut();
+        googleSignOut();
+        navController.navigate(R.id.prfile_to_signup_action);
+    }
+
+    private void googleSignOut(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+        mGoogleSignInClient.signOut();
+    }
 
 }
