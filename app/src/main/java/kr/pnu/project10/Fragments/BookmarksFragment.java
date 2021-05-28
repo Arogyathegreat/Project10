376981d101a975098d1fe6a6725d1f9ed3bfb39d
Arogya Kharel <kharelarogya@hotmail.com>
2021-05-28 17:09:03 +0900

    Custom item for bookmarks added. Added recyclerview to bookmarks_fragment.xml.

--- app/src/main/java/kr/pnu/project10/Fragments/BookmarksFragment.java
+++ app/src/main/java/kr/pnu/project10/Fragments/BookmarksFragment.java
@@ -7,17 +7,35 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.jetbrains.annotations.NotNull;
+
 import kr.pnu.project10.Fragments.ViewModels.BookmarksViewModel;
 import kr.pnu.project10.R;
+import kr.pnu.project10.databinding.BookmarksFragmentBinding;
 
 public class BookmarksFragment extends Fragment {
 
+    private FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+    private FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
     private BookmarksViewModel mViewModel;
+    private BookmarksFragmentBinding binding;
+    private RecyclerView rvBookmarksContainer;
+    private NavController navController;
+
 
     public static BookmarksFragment newInstance() {
         return new BookmarksFragment();
@@ -26,14 +44,38 @@ public class BookmarksFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bookmarks_fragment, container, false);
+        binding = BookmarksFragmentBinding.inflate(inflater, container, false);
+        rvBookmarksContainer = binding.recyclerView;
+        rvBookmarksContainer.setLayoutManager(new LinearLayoutManager(getActivity()));
+        navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(BookmarksViewModel.class);
+        mViewModel.readDB();
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if(mUser != null){
+            setRecyclerView();
+        }
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void setRecyclerView(){
+
+    }
 }
--- app/src/main/java/kr/pnu/project10/Fragments/ViewModels/BookmarksViewModel.java
+++ app/src/main/java/kr/pnu/project10/Fragments/ViewModels/BookmarksViewModel.java
@@ -1,7 +1,35 @@
 package kr.pnu.project10.Fragments.ViewModels;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class BookmarksViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void readDB() {
+        db.collection("Bookmarks")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("HomeFragment", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("HomeFragment", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+
 }
